1575a1f8643ee4eb0e6ffd7a91ab052f0614fb47
vladimir <vovchikbura@mail.ru>
2023-11-29 14:48:04 +0300

    feat: created function concatenateStrings

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -65,8 +65,9 @@ function isString(value) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  // throw new Error('Not implemented');
+  return value1.concat(value2);
 }
 
 /**
