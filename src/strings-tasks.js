e571cb7f98feaf260b16e48a0c9200d27a854ff4
vladimir <vovchikbura@mail.ru>
2023-11-29 17:52:31 +0300

    feat: created function extractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -458,8 +458,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  // throw new Error('Not implemented');
+  return value.replaceAll('Hello, ', '').replaceAll('!', '');
 }
 
 /**
